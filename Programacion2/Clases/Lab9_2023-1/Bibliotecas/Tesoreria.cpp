1d086191e4f5784c8085daec25bec09193c76c94
Carlos Florian <carlosgflorian@gmail.com>
2026-06-04 08:53:01 -0500

    Fix: Incluir carpetas de lectura y reportes en cmake-build-debug

--- Programacion2/Clases/Lab9_2023-1/Bibliotecas/Tesoreria.cpp
+++ Programacion2/Clases/Lab9_2023-1/Bibliotecas/Tesoreria.cpp
@@ -1,5 +1,30 @@
 //
 // Created by Carlos on 4/06/2026.
 //
-
+#include <fstream>
+#include <iostream>
+using namespace std;
 #include "Tesoreria.h"
+
+void Tesoreria::cargaEscalas() {
+    ifstream arch("ArchivoDeDatos/escalas.csv", ios::in);
+    if (not arch.is_open()) {
+        cout << "Error en el archivo de entrada" << endl;
+        exit(1);
+    }
+
+    int cantDatos = 0;
+    while (true) {
+
+        if (arch.eof()) break;
+    }
+}
+
+void Tesoreria::cargaAlumnos() {
+}
+
+void Tesoreria::actualizaBoleta() {
+}
+
+void Tesoreria::imprimeBoleta() {
+}
